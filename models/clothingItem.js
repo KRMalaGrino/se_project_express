d64ab9352dd009ff0289c7657a0bde2c039483ee
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-06-01 16:42:18 -0700

    Created a database and schemas

---
+++ models/clothingItem.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  weather: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  owner: { required: true },
+  likes: {},
+  createdAt: {},
+});
+
+module.exports = mongoose.model("clothingItem", clothingItemSchema);
