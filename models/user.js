e86ce1c1748fb8c179ac1f9d5d9ab239c5e78ea3
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-06-12 16:23:28 -0700

    fixed npm run dev error about the port.

--- models/user.js
+++ models/user.js
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true,
     validate: {
       validator(value) {
         return validator.isEmail(value);
