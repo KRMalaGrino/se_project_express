eeccdcbc81c2d24877008458ed9d65da09ca6104
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-06-12 16:15:24 -0700

    fixed npm run dev errors. fixed auth with no parenthesis. moved token.

--- routes/clothingItems.js
+++ routes/clothingItems.js
@@ -7,15 +7,15 @@ const {
   likeItem,
   dislikeItem,
 } = require("../controllers/clothingItem");
-const { auth } = require("../middlewares/auth");
+const auth = require("../middlewares/auth");
 
 // unprotected route -
 router.get("/", getItems); // readItem
 
-// protected routes -
-router.post("/", auth, createItem); // createItem
-router.delete("/:itemId", auth, deleteItem); // deleteItem
-router.put("/:itemId/likes", auth, likeItem); // likeItem
-router.delete("/:itemId/likes", auth, dislikeItem); // unlike and item
+router.use(auth); // protected routes below
+router.post("/", createItem); // createItem
+router.delete("/:itemId", deleteItem); // deleteItem
+router.put("/:itemId/likes", likeItem); // likeItem
+router.delete("/:itemId/likes", dislikeItem); // unlike and item
 
 module.exports = router;
