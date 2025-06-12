b14ebff2f1fd6d25d4f00488dac9715a08d60f30
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-06-12 09:08:44 -0700

    created middleware auth and added code for authorization

---
+++ middlewares/auth.js
@@ -0,0 +1,31 @@
+const { UNAUTHORIZED } = require("../utils/errors");
+
+const handleAuthError = (res) => {
+  res.status(UNAUTHORIZED).send({ message: "Authorization error" });
+};
+
+const extractBearerToken = (header) => {
+  return header.replace("Bearer ", "");
+};
+
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return handleAuthError(res);
+  }
+
+  const tokenValue = extractBearerToken(authorization);
+  let payload;
+
+  try {
+    payload = jwt.verify(tokenValue, "secrect-token");
+  } catch (err) {
+    return handleAuthError(res);
+  }
+
+  req.user = payload; // adding the payload to the request object
+  next(); // passing the request further along
+};
+
+module.exports = { auth };
