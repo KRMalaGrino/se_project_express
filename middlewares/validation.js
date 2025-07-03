25ea5c58c4bfec16b0852c1fbb1fa77c393ef3b5
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-02 22:58:19 -0700

    updated clothingItemValidation & createUserValidation in validation.js with messages

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,13 @@
 const { Joi, celebrate, Segments } = require("celebrate");
 const validator = require("validator");
 
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
+
 const clothingItemValidation = celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).messages({
@@ -53,13 +60,6 @@ const loginAuthentication = celebrate({
     password: Joi.string().required(),
   }),
 });
-
-const validateURL = (value, helpers) => {
-  if (validator.isURL(value)) {
-    return value;
-  }
-  return helpers.error("string.uri");
-};
 
 module.exports = {
   clothingItemValidation,
