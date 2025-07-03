d4db22e3f7647c167f1158943725457dd04b5859
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-02 22:56:52 -0700

    updated clothingItemValidation & createUserValidation in validation.js with messages

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,19 +1,43 @@
 const { Joi, celebrate, Segments } = require("celebrate");
+const validator = require("validator");
 
 const clothingItemValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required(),
-    imageUrl: Joi.string().uri().required(),
-    weather: Joi.string().valid("hot", "warm", "cold").required(),
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": "Name must be at least 2 characters long",
+      "string.max": "Name must be at most 30 characters long",
+      "string.empty": "The 'name' field must be filled in",
+    }),
+    imageUrl: Joi.string().required().custom(validateURL).messages({
+      "string.empty": "The 'imageUrl' field must be filled in",
+      "string.uri": "The 'imageUrl' field must be a valid URL",
+    }),
+    weather: Joi.string().valid("hot", "warm", "cold").required().messages({
+      "any.only": "The 'weather' field must be one of 'hot', 'warm', or 'cold'",
+      "string.empty": "The 'weather' field must be filled in",
+    }),
   }),
 });
 
 const createUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    avatar: Joi.string().uri().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30).messages({
+      "string.min": "Name must be at least 2 characters long",
+      "string.max": "Name must be at most 30 characters long",
+      "string.empty": "The 'name' field must be filled in",
+    }),
+    avatar: Joi.string().uri().required().messages({
+      "string.uri": "The 'avatar' field must be a valid URL",
+      "string.empty": "The 'avatar' field must be filled in",
+    }),
+    email: Joi.string().email().required().messages({
+      "string.email": "Email must be a valid email address",
+      "string.empty": "The 'email' field must be filled in",
+    }),
+    password: Joi.string().min(8).required().messages({
+      "string.min": "The minimum password length is 8 characters",
+      "string.empty": "The 'password' field must be filled in",
+    }),
   }),
 });
 
@@ -29,6 +53,13 @@ const loginAuthentication = celebrate({
     password: Joi.string().required(),
   }),
 });
+
+const validateURL = (value, helpers) => {
+  if (validator.isURL(value)) {
+    return value;
+  }
+  return helpers.error("string.uri");
+};
 
 module.exports = {
   clothingItemValidation,
