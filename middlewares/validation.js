e4fddb2f07e4582a70c87ce4face4e5c6f882265
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-09 21:47:49 -0700

    fixed case issue in validation.js about the joi to Joi

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -62,7 +62,7 @@ const loginAuthentication = celebrate({
 });
 
 const updateUserValidation = celebrate({
-  body: joi.object().keys({
+  body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required().messages({
       "string.min": "Name must be at least 2 characters long",
       "string.max": "Name must be at most 30 characters long",
