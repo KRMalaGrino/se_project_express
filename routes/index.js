1a5c3dae0bad9e3fddf2606d5c7fb072daa9a6d4
KRMalaGrino <ryanmalagrino9@gmail.com>
2025-07-09 21:30:55 -0700

    added validation dot notation from celebrate to the index routes

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const clothingItems = require("./clothingItems");
 const userRouter = require("./users");
 const { login, createUser } = require("../controllers/user");
 const auth = require("../middlewares/auth");
+const validation = require("../middlewares/validation");
 
-router.post("/signin", login); // unprotected
-router.post("/signup", createUser); // unprotected
+router.post("/signin", validation.loginAuthentication, login); // unprotected
+router.post("/signup", validation.createUserValidation, createUser); // unprotected
 router.use("/items", clothingItems); // protected internally in clothingItems.js
 
 router.use("/users", auth, userRouter); // protected route with auth
